e60fd4d7e9dde5efc5d54953e8c8908d772c4196
SonamWangyel <02230301.cst@rub.edu.bt>
2024-08-22 16:05:34 +0600

    OOP exercise

---
+++ OOPexercise.py
@@ -0,0 +1,17 @@
+# Class definition (blueprint)
+class Dog:
+    def __init__(self, name, color, age):  # 'self' should be the first parameter
+        self.name = name  
+        self.color = color  
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} is {self.age} old and has {self.color} fur."
+
+# Creating objects (instances)
+a = Dog("nima", "black", "1 year")
+b = Dog("dawa", "white", "2 years")
+c = Dog("bruno", "black and white", "2 years")
+
+# Interacting with objects 
+print(c.bark())  # Output: bruno is 2 years old and has black and white fur.
