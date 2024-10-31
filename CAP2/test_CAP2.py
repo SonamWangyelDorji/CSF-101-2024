2673f3f1179cfdd95cefcb6ba8c2cc2ca8c09d5c
SonamWangyel <02230301.cst@rub.edu.bt>
2024-11-01 01:37:18 +0600

    finalized valid_book_borrowing, invalid_book_borrowing and valid_book_returning

--- CAP2/test_CAP2.py
+++ CAP2/test_CAP2.py
@@ -9,8 +9,6 @@ class TestingLibrarySystem(unittest.TestCase):
         self.admin = Admin("ADMIN")
         self.user = User("USER_1")
         self.book = Book("Book","Author")
-        
-
         # Add book in library by admin (Adding two books )
         self.admin.add_books_to_library(self.library_system, self.book)
 
@@ -24,13 +22,25 @@ class TestingLibrarySystem(unittest.TestCase):
         #Check if the borrowed book is in users "Borrowed_book list"
         self.assertIn(self.book, self.user.book_borrowed)
 
-
     #Testing to borrow a book that is alredy borrowed
     def test_invalid_book_borrowing(self):
         self.user.borrow(self.library_system, "Book")
-
         borrowing_same_book = self.user.borrow(self.library_system, "Book")
-        print(borrowing_same_book,"Nothing will be printed")
+
+        # Verify borrowing attempt fails
         self.assertIsNone(borrowing_same_book)
+
+
+    #Testing if user can sucessfully return book and availability status in library and users "borrowed_book" list does not contain the book
+    def test_valid_book_returning(self):
+        self.user.borrow(self.library_system, "Book")
+        self.user.return_book(self.library_system, "Book")
+        #Test if book has been returned
+        self.assertTrue(self.book.availability_status)
+        #Test if the users book_borrowed list is empty
+        self.assertEqual(self.user.book_borrowed, [])
+    
+    #
+
 if __name__ == "__main__":
     unittest.main()
